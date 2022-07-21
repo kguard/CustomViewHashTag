9cd710c6b5d13c8cc76e57f785ee90e84684d691
kguard <kkh9331@naver.com>
2022-07-21 22:00:39 +0900

    version 1.1

--- HashTag/src/main/java/com/example/hashtag/HashTagEditText.kt
+++ HashTag/src/main/java/com/example/hashtag/HashTagEditText.kt
@@ -9,52 +9,56 @@ import android.text.style.ForegroundColorSpan
 import android.text.style.UnderlineSpan
 import android.util.AttributeSet
 import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
 import java.util.regex.Pattern
 
-class HashTagEditText(context: Context, attrs: AttributeSet?) :
-    androidx.appcompat.widget.AppCompatEditText(context,attrs)
+class HashTagEditText(context: Context, attrs: AttributeSet?) : AppCompatEditText(context,attrs)
 {
-        private val mContext: Context = context
-    init
-    {
-        mContext.theme.obtainStyledAttributes(attrs,R.styleable.HashTagEditText,0,0).apply {
-            try {
-                val HashColor=getString(R.styleable.HashTagEditText_HashTag_Color)
-            }
-            finally {
-                recycle()
-            }
+    init {
+        initAttr(attrs)
+        HashTag()
+    }
+    private fun initAttr(attrs: AttributeSet?){
+        context.theme.obtainStyledAttributes(
+            attrs,R.styleable.HashTagEditText,0,0).apply {
+                try {
+                   val editcolor = getString(R.styleable.HashTagEditText_HashTag_Color)
+                }
+                finally {
+                    recycle()
+                }
         }
     }
-
-   /*fun HashTag(editText: EditText){
+    fun HashTag(){
         val hashtag="^#\\w*"
-        editText.addTextChangedListener(object : TextWatcher {
+        this@HashTagEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val a = editText.text.toString().split(" ")
+                val a = this@HashTagEditText.text.toString().split(" ")
                 for (i in a) {
                     val p = Pattern.matches(hashtag, i)
                     if (p) {
-                        editText.text.setSpan(
-                            UnderlineSpan(),
-                            editText.text.indexOf(i),
-                            editText.text.indexOf(i) + i.length,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        editText.text.setSpan(
+                        this@HashTagEditText.text?.indexOf(i)?.let {
+                            this@HashTagEditText.text?.setSpan(
+                                UnderlineSpan(),
+                                it,
+                                it+ i.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        this@HashTagEditText.text?.setSpan(
                             ForegroundColorSpan(Color.BLUE),
-                            editText.text.indexOf(i),
-                            editText.text.indexOf(i) + i.length,
+                            it,
+                            it+ i.length,
                             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
                         )
+                        }
                     }
                 }
             }
             override fun afterTextChanged(s: Editable?) {
             }
         })
-    }*/
+    }
 }
