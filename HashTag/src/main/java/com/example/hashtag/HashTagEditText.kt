ce10115628f4a70e738241e59f6bd48bccb2bf21
kguard <kkh9331@naver.com>
2022-07-24 00:09:49 +0900

    version 1.5

--- HashTag/src/main/java/com/example/hashtag/HashTagEditText.kt
+++ HashTag/src/main/java/com/example/hashtag/HashTagEditText.kt
@@ -16,43 +16,53 @@ class HashTagEditText(context: Context, attrs: AttributeSet?) : AppCompatEditTex
 {
     init {
         initAttr(attrs)
-        HashTag()
+        //HashTag()
     }
     private fun initAttr(attrs: AttributeSet?){
         context.theme.obtainStyledAttributes(
             attrs,R.styleable.HashTagEditText,0,0).apply {
                 try {
                    val editcolor = getString(R.styleable.HashTagEditText_HashTag_Color)
+                    HashTag(Color.parseColor(editcolor))
                 }
                 finally {
                     recycle()
                 }
         }
     }
-    fun HashTag(){
+    fun HashTag(color:Int){
         val hashtag="^#\\w*"
         this@HashTagEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val a = this@HashTagEditText.text.toString().split(" ")
+                var count=0
                 for (i in a) {
                     val p = Pattern.matches(hashtag, i)
-                    if (p) {
-                        this@HashTagEditText.text?.indexOf(i)?.let {
-                            this@HashTagEditText.text?.setSpan(
+                    this@HashTagEditText.text?.indexOf(i,count)?.let {
+                        if (p) {
+                            /*this@HashTagEditText.text?.setSpan(
                                 UnderlineSpan(),
                                 it,
-                                it+ i.length,
+                                it+i.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )*/
+                            this@HashTagEditText.text?.setSpan(
+                                ForegroundColorSpan(color),
+                                it,
+                                it + i.length,
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
                             )
-                        this@HashTagEditText.text?.setSpan(
-                            ForegroundColorSpan(Color.BLUE),
-                            it,
-                            it+ i.length,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
+                            count += i.length+1
+                        }
+                        else {
+                            this@HashTagEditText.text?.setSpan(
+                                ForegroundColorSpan(Color.BLACK),
+                                it,
+                                it + i.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            count += i.length+1
                         }
                     }
                 }
--- app/src/main/java/com/example/hashtag/MainActivity.kt
+++ app/src/main/java/com/example/hashtag/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hashtag
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val editText=findViewById<HashTagEditText>(R.id.edit)
+        //editText.HashTag(Color.BLUE)
     }
 }
